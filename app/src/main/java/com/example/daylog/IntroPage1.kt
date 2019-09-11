d9de68681d0c71d1daedc2b13a4098e7ebe37dca
Vusal <behbudovb@gmail.com>
2019-09-11 22:42:31 +0400

    wqwq

---
+++ app/src/main/java/com/example/daylog/IntroPage1.kt
@@ -0,0 +1,12 @@
+package com.example.daylog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroPage1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_page1)
+    }
+}
---
+++ app/src/main/java/com/example/daylog/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.daylog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar()!!.hide();
+        setContentView(R.layout.splash_screen)
+
+
+    }
+}
